02266e71f8f5748777c7ca209c56bd2d2d761973
Garry Trinder <garry@trinder365.co.uk>
2024-06-25 13:04:27 +0100

    WIP

--- playwright.config.js
+++ playwright.config.js
@@ -45,17 +45,12 @@ module.exports = defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
+    command: "npm run start",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
 });
