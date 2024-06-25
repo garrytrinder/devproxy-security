0b11aa99e3cdbc6dea8c06d9d7b22a928bb5d328
Garry Trinder <garry@trinder365.co.uk>
2024-06-25 14:05:47 +0100

    WIP

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   // load the page
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:3000');
 
   // check if the page contains the splash screen text
   await expect(page.getByRole('paragraph')).toContainText('Product Support Tickets', { timeout: 30000 });
