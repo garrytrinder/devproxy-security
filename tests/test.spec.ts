487d1a809bab21db5af5a08c6bc816cadc2b239b
Garry Trinder <garry@trinder365.co.uk>
2024-06-25 13:20:08 +0100

    WIP

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,9 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  // load the page
   await page.goto('http://localhost:3000/');
-  const popupPromise  = page.waitForEvent('popup');
-  await page.getByRole('button', { name: 'Login' }).click();
+
+  // check if the page contains the splash screen text
+  await expect(page.getByRole('paragraph')).toContainText('Product Support Tickets');
+
+  // check that the login button is visible
+  const button = page.getByRole('button', { name: 'Login' });
+  await expect(button).toBeVisible();
+
+  // set the popup promise
+  const popupPromise = page.waitForEvent('popup');
+
+  // click the login button
+  await button.click();
+
+  // handle the auth popup
   const popup = await popupPromise;
+
+  // after the popup is closed, check if the page contains the last ticket returned in the table
   await expect(page.locator('tbody')).toContainText('Eagle Air Range Limitation Inquiry');
 });
