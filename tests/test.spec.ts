1538131b200a8fa722394686729e88275fef3cb0
Garry Trinder <garry@trinder365.co.uk>
2024-06-25 12:56:04 +0100

    WIP

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-await page.goto('http://localhost:3000/');
-const page1Promise = page.waitForEvent('popup');
+  await page.goto('http://localhost:3000/');
+  const popupPromise  = page.waitForEvent('popup');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page1Promise;
+  const popup = await popupPromise;
   await expect(page.locator('tbody')).toContainText('Eagle Air Range Limitation Inquiry');
 });
